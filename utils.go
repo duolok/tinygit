e00df2c86ef6665c8e005ed7fbf2c162c83d94bb
duolok <dusan.lecic5@gmail.com>
2025-03-09 16:34:57 +0100

    add: creating basic config directory file structure

---
+++ utils.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func CreateConfigDirectory() {
+	if err := ensureDir("./.tinygit"); err != nil {
+		fmt.Println("Directory creation failed. Err: ", err.Error())
+		os.Exit(1)
+	}
+
+	if err := ensureFile("config"); err != nil {
+		fmt.Println("Config creation failed. Err: ", err.Error())
+		os.Exit(1)
+	}
+
+	if err := ensureFile("index"); err != nil {
+		fmt.Println("Index creation failed. Err: ", err.Error())
+		os.Exit(1)
+	}
+}
+
+func ensureDir(dirName string) error {
+	err := os.MkdirAll(dirName, 0777) 
+	if err == nil || os.IsExist(err) {
+		return nil
+	} else {
+		return err
+	}
+}
+
+func ensureFile(fileName string) error {
+	fullPath := filepath.Join("./.tinygit/", fileName)
+
+	_, err := os.Stat(fullPath)
+	if err == nil {
+		return nil 
+	} else if !os.IsNotExist(err) {
+		return err
+	}
+
+	f, err := os.Create(fullPath)
+	if err != nil{
+		return err
+	} 
+	defer f.Close()
+	return nil
+}
