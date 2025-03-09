097274850c81080e0603ff65bd03732c0f8ed6ba
duolok <dusan.lecic5@gmail.com>
2025-03-09 17:42:44 +0100

    add: help uses consts for better readability

--- help.go
+++ help.go
@@ -14,36 +14,36 @@ func PrintHelp() {
 
 func PrintCommandInfo(command string) {
 	switch command {
-	case "--config":
+	case CONFIG:
 		{
 			fmt.Println(`--config   | sets or outputs the name of a commit author
                         [USAGE] ./tinygit --config {NAME}`)
 		}
-	case "--add":
+	case ADD:
 		{
 			fmt.Println(`--add   |  adds a file to the list of tracked files or outputs the list
                         [USAGE] ./tinygit --add {FILE_NAME}`)
 		}
-	case "--log":
+	case LOG:
 		{
 
 			fmt.Println(`--log      | shows all commits
                         [USAGE] ./tinygit --log`)
 		}
-	case "--commit":
+	case COMMIT:
 		{
 
 			fmt.Println(`--commit   | saves the file changes and the author name
                 [USAGE] ./tinygit --commit {FILE_NAME}`)
 		}
 
-	case "--checkout":
+	case CHECKOUT:
 		{
 
 			fmt.Println(`--checkout | allows you to switch between commits and restore previous file state
                 [USAGE] ./tinygit --checkout {COMMIT_HASH}`)
 		}
-	case "--help":
+	case HELP:
 		PrintHelp()
 	default:
 		fmt.Println("Unknown command: ", command, "| try --help for more info")
