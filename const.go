bcb82d82b5244355e814d57b407fabae12a5cb57
duolok <dusan.lecic5@gmail.com>
2025-03-11 08:49:42 +0100

    add: show commit option for showing specific commit

--- const.go
+++ const.go
@@ -1,13 +1,14 @@
 package main
 
 const (
-	CONFIG   = "--config"
-	COMMIT   = "--commit"
-	CHECKOUT = "--checkout"
-	HELP     = "--help"
-	ADD      = "--add"
-	LOG      = "--log"
-	TRACKED  = "--tracked-files"
+	CONFIG      = "--config"
+	COMMIT      = "--commit"
+	CHECKOUT    = "--checkout"
+	HELP        = "--help"
+	ADD         = "--add"
+	LOG         = "--log"
+	TRACKED     = "--tracked-files"
+	SHOW_COMMIT = "--show-commit"
 )
 
 const configPath = "./.tinygit/config"
