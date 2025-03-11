4293c1665c210870e1fea9ed65859efcdba278e6
duolok <dusan.lecic5@gmail.com>
2025-03-11 13:30:35 +0100

    update: add missing show commit flag

--- help.go
+++ help.go
@@ -3,32 +3,33 @@ package main
 import "fmt"
 
 func PrintHelp() {
-	fmt.Println(`[USAGE]        | All TinyGit commands
-                --config        | sets or outputs the name of a commit author
-                --help          | prints the help page
-                --add           | adds a file to the list of tracked files or outputs the list
-                --log           | shows all commits
-                --commit        | saves the file changes and the author name
-                --checkout      | allows you to switch between commits and restore previous file state
+	fmt.Println(`[USAGE]           | All TinyGit commands
+                --config           | sets or outputs the name of a commit author
+                --help             | prints the help page
+                --add              | adds a file to the list of tracked files or outputs the list
+                --log              | shows all commits
+                --commit           | saves the file changes and the author name
+                --checkout         | allows you to switch between commits and restore previous file state
+                --show-commit      | allows you to switch between commits and restore previous file state
 		--tracked-files | shows all currently tracked files`)
 }
 
 func PrintCommandInfo(command string) {
 	switch command {
 	case CONFIG:
-			fmt.Println(`--config   | sets or outputs the name of a commit author
+		fmt.Println(`--config   | sets or outputs the name of a commit author
                         [USAGE] ./tinygit --config {NAME}`)
 	case ADD:
-			fmt.Println(`--add   |  adds a file to the list of tracked files or outputs the list
+		fmt.Println(`--add   |  adds a file to the list of tracked files or outputs the list
                         [USAGE] ./tinygit --add {FILE_NAME}`)
 	case LOG:
-			fmt.Println(`--log      | shows all commits
+		fmt.Println(`--log      | shows all commits
                         [USAGE] ./tinygit --log`)
 	case COMMIT:
-			fmt.Println(`--commit   | saves the file changes and the author name
+		fmt.Println(`--commit   | saves the file changes and the author name
                 [USAGE] ./tinygit --commit {FILE_NAME}`)
 	case CHECKOUT:
-			fmt.Println(`--checkout | allows you to switch between commits and restore previous file state
+		fmt.Println(`--checkout | allows you to switch between commits and restore previous file state
                 [USAGE] ./tinygit --checkout {COMMIT_HASH}`)
 	case HELP:
 		PrintHelp()
